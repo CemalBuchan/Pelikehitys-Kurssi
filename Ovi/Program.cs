9219239d41cea188c98520350b3e2a7c52eccbdf
CemalBuchan <2rftmv4d6s@privaterelay.appleid.com>
2026-01-13 15:53:14 +0200

    13,01 Whole day

---
+++ Ovi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ovi
+{
+    internal class Program
+    {
+        public enum doorState {Opened, Closed, Locked}
+       // it will be easyer if i use switch but why not "if" :)
+        static void Main(string[] args)
+        {
+            doorState state = doorState.Opened;
+            
+            Console.WriteLine("You can open, close, lock, unlock");
+            while (true)
+            {
+                Console.WriteLine("Door Is "+state);
+                Console.Write("what do you want to do  ");
+                string Want = Console.ReadLine();
+
+
+                if (state == doorState.Opened && Want == "close")
+                {
+                    state = doorState.Closed;
+                }
+                else if(state == doorState.Opened && Want != "close")
+                {
+                    Console.WriteLine("You cant do it");
+                }
+
+
+                else if(state == doorState.Closed && Want == "open")
+                {
+                    state = doorState.Opened;
+                }
+                else if(state == doorState.Closed && Want == "lock")
+                {
+                    state = doorState.Locked;
+                }
+                else if (state == doorState.Closed && Want == "close")
+                {
+                    Console.WriteLine("You cant do it");
+                } 
+
+
+                else if(state == doorState.Locked && Want == "unlock")
+                {
+                    state=doorState.Closed;
+                }
+                else if( state == doorState.Locked && Want != "unlock")
+                {
+                    Console.WriteLine("You cant do it");
+                }
+
+
+                else if (Want != "close" || Want != "open" || Want != "lock")
+                {
+                    Console.WriteLine("You cant do it");
+                }
+
+            }
+        }
+    }
+}
---
+++ Ruoka-annos/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace Ruoka_annos
+{
+    internal class Program
+    {
+        public enum main { Chicken, Meat, Vegtables}
+        public enum side { Potato, Rice, Pasta}
+        public enum sauce { Curry, Chili, Tomato}
+
+
+        static void Main(string[] args)
+        {
+            main mainDish = main.Chicken;
+            Console.WriteLine("Chose your Food from menu with numbers in the menu");
+            Console.WriteLine("Main dishes ( 1-Chicken, 2-Meat, 3-Vegtables )");
+            string mainInt;
+            string sideInt;
+            string sauceInt;
+
+            while (true)
+            {
+                mainInt = Console.ReadLine();
+
+                if(mainInt == "1" || mainInt == "2" || mainInt == "3")
+                    break;
+
+                Console.WriteLine("Chose number from menu");
+
+            }
+
+            switch(mainInt)
+            {
+                case "1":
+                    mainDish = main.Chicken;
+                    break;
+                case "2":
+                    mainDish = main.Meat;
+                    break;
+                case "3":
+                    mainDish = main.Vegtables;
+                    break;
+
+            }
+
+
+
+            side sideDish = side.Potato;
+            Console.WriteLine("Side dishes ( 1-Potato, 2-Rice, 3-Pasta )");
+
+            while (true)
+            {
+                sideInt = Console.ReadLine();
+
+                if (sideInt == "1" || sideInt == "2" || sideInt == "3")
+                    break;
+
+                Console.WriteLine("Chose number from menu");
+
+            }
+
+            switch (sideInt)
+            {
+                case "1":
+                    sideDish = side.Potato;
+                    break;
+                case "2":
+                    sideDish = side.Rice;
+                    break;
+                case "3":
+                    sideDish = side.Pasta;
+                    break;
+            }
+
+            sauce sauces = sauce.Curry;
+            Console.WriteLine("Sauces ( 1-Curry, 2-Chilli, 3-Tomato )");
+
+
+            while (true)
+            {
+                sauceInt = Console.ReadLine();
+
+                if (sauceInt == "1" || sauceInt == "2" || sauceInt == "3")
+                    break;
+
+                Console.WriteLine("Chose number from menu");
+
+            }
+
+
+            switch (sauceInt)
+            {
+                case "1":
+                    sauces = sauce.Curry;
+                    break;
+                case "2":
+                    sauces = sauce.Chili;
+                    break;
+                case "3":
+                    sauces = sauce.Tomato;
+                    break;
+            }
+
+            Console.WriteLine(mainDish+ " and " +sideDish+ " with " +sauces);
+
+
+
+        }
+    }
+}
